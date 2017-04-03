7045e88b5a1052efc19a005a84d95b503bea39e2
Mateusz Nowak <mateusz.nowak@swmansion.com>
2017-04-03 10:48:07 +0200

    Using bundlex

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Membrane.Element.PortAudio.Mixfile do
 
   def project do
     [app: :membrane_element_portaudio,
-     compilers: ["membrane.compile.c"] ++ Mix.compilers,
+     compilers: ~w(bundlex.lib) ++ Mix.compilers,
      version: "0.0.1",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
@@ -33,6 +33,7 @@ defmodule Membrane.Element.PortAudio.Mixfile do
       {:membrane_core, git: "git@github.com:membraneframework/membrane-core.git"},
       {:membrane_common_c, git: "git@github.com:membraneframework/membrane-common-c.git"},
       {:membrane_caps_audio_raw, git: "git@github.com:membraneframework/membrane-caps-audio-raw.git"},
+      {:bundlex, git: "git@github.com:radiokit/bundlex.git"},
     ]
   end
 end
