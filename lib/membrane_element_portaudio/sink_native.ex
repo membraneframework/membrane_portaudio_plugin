a57d74665323629e48515d8769af0afa0b038cfb
Mateusz Front <mateusz.front@swmansion.com>
2018-05-17 14:07:37 +0200

    fixed native sink typespecs

--- lib/membrane_element_portaudio/sink_native.ex
+++ lib/membrane_element_portaudio/sink_native.ex
@@ -3,11 +3,11 @@ defmodule Membrane.Element.PortAudio.Sink.Native do
 
   use Bundlex.Loader, nif: :sink
 
-  @spec create(pid, integer, pos_integer, pos_integer, :low | :high) :: {:ok, any} | {:error, any}
+  @spec create(pid, integer, pos_integer, pos_integer, :low | :high) ::
+          {:ok, reference} | {:error, any}
   defnif create(demand_handler, endpoint_id, ringbuffer_size, pa_buffer_size, latency)
 
-  @spec write(any, %Membrane.Buffer{}) ::
-          :ok | {:error, {:args, atom, String.t()}} | {:error, {:internal, atom}}
+  @spec write(reference, binary) :: :ok | {:error, any}
   defnif write(handle, buffer)
 
   @spec get_default_endpoint_id() :: integer
