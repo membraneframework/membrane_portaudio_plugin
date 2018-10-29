4d5aee769822317089b4749e3f50bf1ff4bf689c
Mateusz Front <mateusz.front@swmansion.com>
2018-10-29 12:09:36 +0100

    increased elixir and OTP versions in travis and mix.exs

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Membrane.Element.PortAudio.Mixfile do
       app: :membrane_element_portaudio,
       compilers: [:unifex, :bundlex] ++ Mix.compilers(),
       version: @version,
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       description: "Membrane Multimedia Framework (PortAudio Element)",
       package: package(),
