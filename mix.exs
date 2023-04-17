8b75a391e84d46c07f3f3614478be00b9a3cc28b
Mateusz Front <mateusz.front@swmansion.com>
2023-04-17 12:04:28 +0200

    Fix bunch dependency version pattern

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Membrane.PortAudio.Mixfile do
     [
       {:membrane_core, "~> 0.11.2"},
       {:membrane_common_c, "~> 0.14.0"},
-      {:bunch, "~> 1.5.0"},
+      {:bunch, "~> 1.5"},
       {:unifex, "~> 1.0"},
       {:membrane_raw_audio_format, "~> 0.10.0"},
       {:bundlex, "~> 1.0"},
