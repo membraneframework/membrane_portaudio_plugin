381e29d7a7674319e66c5f81576f3dd2ae412706
Mateusz Front <mateusz.front@swmansion.com>
2024-07-10 18:58:39 +0200

    bump version to 0.19.3

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Membrane.PortAudio.Mixfile do
   use Mix.Project
 
   @github_url "https://github.com/membraneframework/membrane_portaudio_plugin"
-  @version "0.19.2"
+  @version "0.19.3"
 
   def project do
     [
