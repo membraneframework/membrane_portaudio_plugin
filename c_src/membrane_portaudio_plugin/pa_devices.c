3f3ed5502eb7cc32b396d757c6608c909003dd0d
Mateusz Front <mateusz.front@swmansion.com>
2024-07-10 18:36:59 +0200

    format pa_devices.c

--- c_src/membrane_portaudio_plugin/pa_devices.c
+++ c_src/membrane_portaudio_plugin/pa_devices.c
@@ -1,43 +1,30 @@
 #include "pa_devices.h"
 
-UNIFEX_TERM list(UnifexEnv *env)
-{
+UNIFEX_TERM list(UnifexEnv *env) {
   Pa_Initialize();
   int numDevices = Pa_GetDeviceCount();
   device devices[numDevices];
-  if (numDevices < 0)
-  {
+  if (numDevices < 0) {
     printf("\nERROR: Pa_CountDevices returned 0x%x\n", numDevices);
     return list_result(env, devices, numDevices);
-  }
-  else if (numDevices == 0)
-  {
+  } else if (numDevices == 0) {
     return list_result(env, devices, numDevices);
-  }
-  else
-  {
+  } else {
     int default_input_id = Pa_GetDefaultInputDevice();
     int default_output_id = Pa_GetDefaultOutputDevice();
-    for (int i = 0; i < numDevices; i++)
-    {
+    for (int i = 0; i < numDevices; i++) {
       const PaDeviceInfo *device_info = Pa_GetDeviceInfo(i);
 
-      if (i == default_input_id)
-      {
+      if (i == default_input_id) {
         devices[i].default_device = DEFAULT_DEVICE_INPUT;
-      }
-      else if (i == default_output_id)
-      {
+      } else if (i == default_output_id) {
         devices[i].default_device = DEFAULT_DEVICE_OUTPUT;
-      }
-      else
-      {
+      } else {
         devices[i].default_device = DEFAULT_DEVICE_FALSE;
       }
 
       devices[i].name = malloc(strlen(device_info->name) + 1);
-      if (device_info->name != NULL)
-      {
+      if (device_info->name != NULL) {
         strcpy(devices[i].name, device_info->name);
       }
 
